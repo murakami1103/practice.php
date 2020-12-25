c29db46037bc80649141554bcd2be1d91c763d93
村上晴 <hkw355@gmai.com>
2020-12-25 03:02:41 +0000

    commit

---
+++ practice.php
@@ -0,0 +1,18 @@
+<?php
+$a = 3;
+$b = 7;
+echo $a + $b;
+
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month [7];
+
+$hello = "Hello,";
+$name = "murakami_haru";
+$world = "'s World!";
+echo $hello . $name . $world;
+
+$tech_boost = "tech";
+$tech_boost .= "boost";
+echo $tech_boost;
+?>
+
