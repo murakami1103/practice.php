2ebe1a19fa0e1a0774502f01e411913b60dfd36e
村上晴 <hkw355@gmai.com>
2020-12-29 10:08:46 +0000

    commit

--- practice04.php
+++ practice04.php
@@ -1,34 +1,50 @@
 <?php
 
 function double($x){
-    echo $x * 2;
-    echo "\n";
+    return $x * 2;
 }
+$wnum = double(4);
+echo $wnum;
+echo "\n";
+
 
 
 function f($a, $b){
-    echo $a + $b;
-    echo "\n";
+    return $a + $b;
 }
+$num = f(3,5);
+echo $num;
+echo "\n";
 
 
-$arr = array(1 ,3 ,5 ,7 ,9);
-function f2($arr){
-    
+$arr = array(1 ,20 ,5 ,7 ,9);
+function f2($x){
+    $result = 1;
+    foreach($x as $a){
+        $result = $result * $a;
+        echo $result;
+        echo "\n";
+    }
+    return $result;
 }
-
+f2($arr);
 
 function max_array($arr){
     $max_number = $arr[0];
     foreach($arr as $a){
-        
+        if($max_number < $a){
+            $max_number = $a;
+            
+        }
     }
     return $max_number;
 }
+echo max_array($arr);
 
 
 $text = '<p>あいうえお</p>';
 echo strip_tags($text);
+$text = strip_tags($text);
 echo $text;
 echo "\n";
 
@@ -36,7 +52,7 @@ echo "\n";
 $colors = array("rad" ,"blue");
 array_push ($colors ,"green" ,"orange");
 print_r ($colors);
-
+echo "\n";
 
 $animal1 = ["dog" ,"cat"];
 $animal2 = ["pig" ,"mouse"];
@@ -52,6 +68,13 @@ echo mktime (8 ,30 ,0 ,12 ,27 ,2020);
 echo "\n";
 
 echo date ("y/m/d");
+echo "\n";
+
+
+function tax($x){
+    return $x * 1.05;
+}
+echo tax(200);
 echo "\n";
 
 
